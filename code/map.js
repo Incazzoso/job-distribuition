1052502d10383fff8ecb85010df9a504fc7bbde5
Gabriele Bigatti <bigattigabriele28@gmail.com>
2025-11-20 11:54:59 +0100

    fixed the colored regions of the map

--- code/map.js
+++ code/map.js
@@ -1,22 +1,21 @@
-const map = L.map('map').setView([42.2, 12.5], 6);
+const map = L.map('map').setView([41.9, 12.5], 6);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-fetch('./js/limits_IT_regions.geojson')
-    .then(res => res.json())
+let geojson;
+
+fetch('../code/limits_IT_regions.geojson')
+    .then(response => response.json())
     .then(data => {
     L.geoJSON(data, {
-        style: function(feature) {
-        return {
-            color: '#333',
-            fillColor: '#66ccff',
-            fillOpacity: 0.5
-        };
-    },
-        onEachFeature: function(feature, layer) {
-            layer.bindPopup('<strong>' + feature.properties.nome + '</strong>');
+            style: {
+            color: "#3388ff",
+            weight: 2
+        },
+    onEachFeature: function (feature, layer) {
+        layer.bindPopup(feature.properties.reg_name);
         }
     }).addTo(map);
 });
